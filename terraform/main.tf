2f8ad338d0774c3315d0f3719f7ab1892e521959
emmanuel.pius-ogiji <emmanuel.pius-ogiji@askattest.com>
2025-10-15 16:15:43 +0100

    Step 1 Deploy AWS Control Tower and the Landing [sc-136343] - feat: Add workload accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,9 +25,99 @@ module "shared_services" {
   }
 
   custom_fields = {
-    group = "infrastructure"
+    group       = "infrastructure"
     description = "Shared infrastructure for the organization like DNS, networking etc"
   }
 
   account_customizations_name = "shared-services"
+}
+
+module "dev" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-attest-ct+dev@askattest.com"
+    AccountName               = "${local.account_name_prefix}-dev"
+    ManagedOrganizationalUnit = "Workloads"
+    SSOUserEmail              = "emmanuel.pius-ogiji@askattest.com"
+    SSOUserFirstName          = "Emmanuel"
+    SSOUserLastName           = "Pius-Ogiji"
+  }
+
+  account_tags = {
+    Name      = "dev",
+    ManagedBy = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Emmanuel Pius-Ogiji"
+    change_reason       = "https://app.shortcut.com/attest/story/136343/step-1-deploy-aws-control-tower-and-the-landing-zone"
+  }
+
+  custom_fields = {
+    group       = "workloads"
+    description = "Workloads: Development"
+  }
+
+  account_customizations_name = "dev"
+}
+
+module "qa" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-attest-ct+qa@askattest.com"
+    AccountName               = "${local.account_name_prefix}-qa"
+    ManagedOrganizationalUnit = "Workloads"
+    SSOUserEmail              = "emmanuel.pius-ogiji@askattest.com"
+    SSOUserFirstName          = "Emmanuel"
+    SSOUserLastName           = "Pius-Ogiji"
+  }
+
+  account_tags = {
+    Name      = "qa",
+    ManagedBy = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Emmanuel Pius-Ogiji"
+    change_reason       = "https://app.shortcut.com/attest/story/136343/step-1-deploy-aws-control-tower-and-the-landing-zone"
+  }
+
+  custom_fields = {
+    group       = "workloads"
+    description = "Workloads: QA"
+  }
+
+  account_customizations_name = "qa"
+}
+
+module "prod" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-attest-ct+prod@askattest.com"
+    AccountName               = "${local.account_name_prefix}-prod"
+    ManagedOrganizationalUnit = "Workloads"
+    SSOUserEmail              = "emmanuel.pius-ogiji@askattest.com"
+    SSOUserFirstName          = "Emmanuel"
+    SSOUserLastName           = "Pius-Ogiji"
+  }
+
+  account_tags = {
+    Name      = "prod",
+    ManagedBy = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Emmanuel Pius-Ogiji"
+    change_reason       = "https://app.shortcut.com/attest/story/136343/step-1-deploy-aws-control-tower-and-the-landing-zone"
+  }
+
+  custom_fields = {
+    group       = "workloads"
+    description = "Workloads: Production"
+  }
+
+  account_customizations_name = "prod"
 }
